46b8001e3a80ff3810730f0d7c844696708c9fc5
jcgonzalezgusev <jcgonzalezgusev@gmail.com>
2021-06-19 17:10:43 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        String message = "Hello World";
+        System.out.println(message + " y tu dices: " + args[0]);
+    }
+}
